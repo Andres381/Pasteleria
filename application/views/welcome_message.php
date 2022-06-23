7e981edf605d8a1e768958ce5d48d2330aee6760
Xia23 <xiaherrera1023@gmail.com>
2022-06-22 19:31:40 -0500

    Prueba 3

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -92,6 +92,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="userguide3/">User Guide</a>.</p>
 	</div>
+<p> Prueba 3</p>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
